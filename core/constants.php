f934e797f8dd6c86ac87d982c165ce9b303232ac
Haydar Kulekci <haydarkulekci@gmail.com>
2013-07-08 00:48:34 +0300

    muhafiz test

--- core/constants.php
+++ core/constants.php
@@ -1,17 +1,27 @@
 <?php
-	define("_PAGE_CHARSET","utf-8");
+/**
+ *   File Document
+ *
+ *   PHP version 5
+ * 
+ *   @category Category
+ *   @package  Package
+ *   @author   Display Name <username@example.com>
+ *   @license  GPLv3 - http://test.com
+ *   @link     http://test.com
+ *
+ */
 
-	/*
+define("_PAGE_CHARSET", "utf-8");
+
+/*
 Burada tanımlanan sabitler sistem 
 içerisinde başka yerlerde kullanılıyor.
-	*/
+*/
 
-	// <<<<< connect.php//
-	define("_DB_LOCALHOST","localhost");
-	define("_DB_USERNAME","demo");
-	define("_DB_PASSWORD","demo");
-	define("_DB_DATABASENAME","demo_");
-	define("_DB_HATAMESAJI","_DB");
-
-
-?>
+// <<<<< connect.php//
+define("_DB_LOCALHOST", "localhost");
+define("_DB_USERNAME", "demo");
+define("_DB_PASSWORD", "demo");
+define("_DB_DATABASENAME", "demo_");
+define("_DB_HATAMESAJI", "_DB");
